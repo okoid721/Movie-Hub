459e71bff3e00712abec6a1145404b8ade49469d
okoid721 <okoid721@gmail.com>
2024-03-19 00:34:42 +0100

    Movie-hub

--- components/CategoryList.tsx
+++ components/CategoryList.tsx
@@ -6,13 +6,14 @@ interface Props {
   movies: Movie[];
 }
 
+const CategoryList = ({ title, movies }: Props) => {
   return (
     <div className="category">
       <h1 className="category-title">{title}</h1>
       <div className="movie-list">
-          {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-          ))}
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </div>
     </div>
   );
