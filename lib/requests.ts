592d5ef93347aea8eef0365e4d5be8b861481e1d
okoid721 <okoid721@gmail.com>
2024-03-20 16:39:27 +0100

    Movie-hub

--- lib/requests.ts
+++ lib/requests.ts
@@ -17,19 +17,3 @@ export async function getApiResponse(sub_url: string) {
     return Promise.reject(error);
   }
 }
-
-// const fetch = require('node-fetch');
-
-// const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzQyZTllNzFmNTkxY2RiNDM2NTZmOTEyMWUwZTEwMSIsInN1YiI6IjY1Zjg1YzgyZDhmNDRlMDE2MzUwYTA3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iffut1Bf05HXJII07e1B74oZM6sHBgpQuBA79T2_iHA'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error('error:' + err));
